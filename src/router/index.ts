2c57d82999bdd365dbbbe2e86b21cea555293427
Sascha Trifunovic <sascha.trifunovic@gmail.com>
2022-11-29 11:50:36 +0100

    Fix infinite loop and other deploy issues

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,9 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: {
+        requiresAuth: false,
+      },
       children: [
         {
           path: "contacts",
@@ -37,14 +40,6 @@ const router = createRouter({
         },
       ],
     },
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import("../views/WallView.vue"),
-    // },
   ],
 });
 
